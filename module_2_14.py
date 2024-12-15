c205d0a9ef500d454ec25a961a9a22e6305c81b0
Billy <iceqlips@gmail.com>
2024-12-15 15:22:01 +0500

    module 2 lesson 14

---
+++ module_2_14.py
@@ -0,0 +1,25 @@
+        # Я попробую пропускать строки. Хочу услышать фидбэк, так лучше код читается или нет?
+        # Моё мнение нет =)
+        # Просто где то пропускают, где то нет, и я пытаюсь узнать, как всё же лучше?
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number == 1:
+        continue
+    is_prime = True
+
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(number)
+
+    else:
+        not_primes.append(number)
+
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
